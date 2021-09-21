f4620dfdfa3c350a1838bc3fe09d4044212ceb09
m0rph1u5 <80258636+m0rph1u5@users.noreply.github.com>
2021-09-21 11:57:20 -0700

    0.0.2

--- CustomCheckIn.py
+++ CustomCheckIn.py
@@ -1,22 +1,68 @@
 # Custom COVID-19 Check-In : QR Generator
+
+import replit
+import time
 import qrcode
 from PIL import Image
-print("Custom COVID-19 Check-In : QR Generator \n***USE AT OWN RISK***")
 
-# Input
-location = str(input("Enter a Location"))
-locat = location.replace(" ", "+")
-checkin = ("https://checkin.covid-19.sa.gov.au/?businessName="+ (locat) +"&planId=000000000000000000000000")
+def welcome():
+    choice = 0
+    while choice <= 3:
+        print("\nCustom COVID-19 Check-In & QR Generator")
 
-# Combination
+        try:
+            print("\n1. Generate Check-In Link \n2. Generate Check-In QR \n3. Exit ")
+            choice = int(input("\nSelect Generator: "))
 
-print("https://checkin.covid-19.sa.gov.au/?businessName="+ (locat) +"&planId=000000000000000000000000")
+            if choice == 1:
+                location = str(input("Enter a Location"))
+                locat = location.replace(" ", "+")
+                checkin = ("https://checkin.covid-19.sa.gov.au/?businessName=" + (
+                    locat) + "&planId=000000000000000000000000")
 
-end = input("\nWould you like to Generate a QR Code? Y/N ")
-if end == "y":
-    img = qrcode.make(checkin)
-    img.save((location) + ".png")
-if end == "n":
-    print("\n\nProgram terminated")
+                # Combination
+
+                print(
+                    "https://checkin.covid-19.sa.gov.au/?businessName=" + (locat) + "&planId=000000000000000000000000")
+
+            elif choice == 2:
+                location = str(input("Enter a Location"))
+                locat = location.replace(" ", "+")
+                checkin = ("https://checkin.covid-19.sa.gov.au/?businessName=" + (
+                locat) + "&planId=000000000000000000000000")
+
+                # Combination
+
+                print("https://checkin.covid-19.sa.gov.au/?businessName=" + (locat) + "&planId=000000000000000000000000")
+                img = qrcode.make(checkin)
+                img.save((location) + ".png")
+
+            elif choice == 3:
+                replit.clear()
+                print("\n\nProgram terminated")
+                return
+            else:
+                print("\nWrong selection, please choose options 1-3")
+                time.sleep(2)
+                replit.clear()
+                welcome()
+
+            end = input("\nWould you like to continue Y/N ")
+            if end == "y":
+                replit.clear()
+            if end == "n":
+                replit.clear()
+                print("\n\nProgram terminated")
+                return
+            elif end == "3":
+                replit.clear()
+                print("\n\nProgram terminated")
+                return
+
+        except ValueError:
+            print("\nPlease enter a number!")
+            time.sleep(1)
+            replit.clear()
 
 
+welcome()
